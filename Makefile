332d4cbf07e933126e4c5b6db5982354ee45e3c4
Joey Hess <joeyh@debian.org>
2005-07-02 16:09:39 +0000

    * Add debcommit, a script to commit to svn (or cvs) based on the contents of   the changelog, and svnpath, which it uses to generate svn tags.   Closes: #316577

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@
 
 PL_FILES = bts.pl checkbashisms.pl cvs-debuild.pl debchange.pl \
 	debdiff.pl debi.pl debpkg.pl debuild.pl dpkg-depcheck.pl \
-	dscverify.pl grep-excuses.pl plotchangelog.pl rc-alert.pl
+	dscverify.pl grep-excuses.pl plotchangelog.pl rc-alert.pl \
+	debcommit.pl svnpath.pl
 
 SH_FILES = cvs-debi.sh cvs-debrelease.sh debclean.sh debrelease.sh \
 	debrsign.sh debsign.sh dpkg-genbuilddeps.sh mergechanges.sh \
@@ -19,7 +20,7 @@ SCRIPTS = $(PL_FILES:.pl=) $(SH_FILES:.sh=)
 EXAMPLES = conf.default
 
 MAN1S = $(SCRIPTS:=.1) debc.1 cvs-debc.1 devscripts.1
-GEN_MAN1S = bts.1
+GEN_MAN1S = bts.1 svnpath.1 debcommit.1
 MAN5S = devscripts.conf.5
 
 BINDIR = /usr/bin
@@ -54,7 +55,7 @@ conf.default: conf.default.in version
 	VERSION=`cat version` && sed -e "s/###VERSION###/$$VERSION/" $< \
 	    > $@.tmp && mv $@.tmp $@
 
-bts.1: bts.pl
+%.1: %.pl
 	pod2man --center=" " --release="Debian Utilities" $< > $@
 
 libvfork.o: libvfork.c
---
+++ debcommit.pl
@@ -0,0 +1,179 @@
+#!/usr/bin/perl
+
+=head1 NAME debcommit
+
+debcommit - commit changes to a package
+
+=head1 SYNOPSIS
+
+debcommit [--release] [--message=text] [--noact]
+
+=head1 DESCRIPTION
+
+debcommit generates a commit message based on new text in debian/changelog,
+and commits the change to a package's cvs or svn repository. It must be run
+in a cvs or svn working copy for the package.
+
+=head1 OPTIONS
+
+=over 4
+
+=item -r --release
+
+Commit a release of the package. The version number is determined from
+debian/changelog, and is used to tag the package in cvs or svn.
+
+Note that svn tagging conventions vary, so debcommit uses
+L<svnpath(1)> to determine where the tag should be placed in the
+repository.
+
+=item -m text --message test
+
+Specify a commit message to use. Useful if the program cannot determine
+a commit message on its own based on debian/changelog, or if you want to
+override the default message.
+
+=item -n --noact
+
+Do not actually do anything, but do print the commands that would be run.
+
+=over 4
+
+=back
+
+=cut
+
+use warnings;
+use strict;
+use Getopt::Long;
+
+my $release=0;
+my $message;
+my $noact=0;
+if (! GetOptions(
+	"release" => \$release,
+	"message=s" => \$message,
+	"noact" => \$noact,
+)) {
+	die "Usage: debcommit [--release] [--message=text] [--noact]\n";
+}
+
+my $prog=getprog();
+if (! -e "debian/changelog") {
+	die "cannot find debian/changelog\n";
+}
+
+if ($release) {
+	open (C, "<debian/changelog") || die "cannot read debian/changelog: $!";
+	my $top=<C>;
+	if ($top=~/UNRELEASED/) {
+		die "debian/changelog says it's UNRELEASED\n";
+	}
+	close C;
+
+	my $version=`dpkg-parsechangelog | grep Version: | cut -f 2 -d ' '`;
+	chomp $version;
+
+	$message="releasing version $version" if ! defined $message;
+	commit($message);
+	tag($version);
+}
+else {
+	$message=getmessage() if ! defined $message;
+	commit($message);
+}
+
+sub getprog {
+	if (-d ".svn") {
+		return "svn";
+	}
+	elsif (-d "CVS") {
+		return "CVS";
+	}
+	else {
+		die "not in a cvs or subversion working copy\n";
+	}
+}
+
+sub action {
+	my $prog=shift;
+	print $prog." ".join(" ", map { if (/[^-A-Za-z0-9]/) { "'$_'" } else { $_ } } @_)."\n";
+	return 1 if $noact;
+	if (system($prog, @_) != 0) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+sub commit {
+	my $message=shift;
+
+	if ($prog eq 'cvs' || $prog eq 'svn') {
+		if (! action($prog, "commit", "-m", $message)) {
+			die "commit failed\n";
+		}
+	}
+	else {
+		die "unknown program $prog";
+	}
+}
+
+sub tag {
+	my $tag=shift;
+
+	if ($prog eq 'svn') {
+		my $svnpath=`svnpath`;
+		chomp $svnpath;
+		my $tagpath=`svnpath tags`;
+		chomp $tagpath;
+		
+		if (! action("svn", "copy", $svnpath, "$tagpath/$tag", "-m", "tagging version $tag") ||
+		    ! action("svn", "mkdir", $tagpath) || # make tag subdir
+		    ! action("svn", "copy", $svnpath, "$tagpath/$tag", "-m", "tagging version $tag")) {
+			die "failed tagging with $tag\n";
+		}
+	}
+	elsif ($prog eq 'cvs') {
+		$tag=~s/^[0-9]+://; # strip epoch
+		$tag=~tr/./_/;      # mangle for cvs
+		if (! action("cvs", "tag", "-f", $tag)) {
+			die "failed tagging with $tag\n";
+		}
+	}
+}
+
+sub getmessage {
+	my $ret;
+
+	if ($prog eq 'cvs' || $prog eq 'svn') {
+		$ret='';
+		foreach my $line (`$prog diff debian/changelog`) {
+			next unless $line=~/^\+  /;
+			$line=~s/^\+  //;
+			next if $line=~/^\s*\[.*\]\s*$/; # maintainer name
+			$ret.=$line;
+		}
+		
+		if (! length $ret) {
+			die "Unable to determine commit message using $prog\nTry using the -m flag.\n";
+		}
+	}
+	else {
+		die "unknown program $prog";
+	}
+
+	chomp $ret;
+	return $ret;
+}
+
+=head1 LICENSE
+
+GPL
+
+=head1 AUTHOR
+
+Joey Hess <joeyh@debian.org>
+
+=cut
