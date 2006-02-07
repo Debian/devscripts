616d7b5c5f9c0a2461557e5a881936b21a1120fd
Julien Gilbey <jdg@debian.org>
2006-02-07 19:00:51 +0000

    * nmudiff: improved old version code from Steinar using supplied patch   (Closes: #351791)   (Closes: #347671)

--- nmudiff.sh
+++ nmudiff.sh
@@ -9,10 +9,16 @@ fi
 
 SOURCE=$( dpkg-parsechangelog | grep ^Source: | cut -d" " -f2 )
 VERSION=$( dpkg-parsechangelog | grep ^Version: | cut -d" " -f2 )
-OLDVERSION=$( dpkg-parsechangelog -v~ | sed -n "s/^ [^ .][^ ]* (\(.*\)).*$/\1/p" | head -2 | tail -1 )
+OLDVERSION=$( dpkg-parsechangelog -v~ \
+  | sed -n "s/^ [^ .][^ ]* (\(.*\)).*$/\1/p" | head -2 | tail -1 )
 
-debdiff ../${SOURCE}_$OLDVERSION.dsc ../${SOURCE}_$VERSION.dsc > ../${SOURCE}-$VERSION-nmu.diff
-sensible-editor ../${SOURCE}-$VERSION-nmu.diff
+VERSION_NO_EPOCH=$( echo "$VERSION" | sed "s/^[0-9]\+://" )
+OLDVERSION_NO_EPOCH=$( echo "$OLDVERSION" | sed "s/^[0-9]\+://" )
+
+debdiff ../${SOURCE}_${OLDVERSION_NO_EPOCH}.dsc \
+  ../${SOURCE}_${VERSION_NO_EPOCH}.dsc \
+  > ../${SOURCE}-${VERSION_NO_EPOCH}-nmu.diff
+sensible-editor ../${SOURCE}-${VERSION_NO_EPOCH}-nmu.diff
 
 TMPNAM=$( tempfile )
 cat <<EOF >$TMPNAM
@@ -26,6 +32,7 @@ Hi,
 Attached is the diff for my $SOURCE $VERSION NMU.
 EOF
 
-mutt -s "diff for $VERSION NMU" -i $TMPNAM -a ../${SOURCE}-$VERSION-nmu.diff submit@bugs.debian.org
+mutt -s "diff for $VERSION NMU" -i $TMPNAM \
+  -a ../${SOURCE}-${VERSION_NO_EPOCH}-nmu.diff submit@bugs.debian.org
 rm $TMPNAM
 
