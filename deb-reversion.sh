7c9bbc3c28d7acdfeb605f1e16e2ecefc8d33f35
Julien Gilbey <jdg@debian.org>
2006-11-09 01:04:41 +0000

    change formatting

--- deb-reversion.sh
+++ deb-reversion.sh
@@ -6,8 +6,9 @@
 # Please see http://www.opensource.org/licenses/artistic-license.php for more
 # information.
 #
-# deb-reversion (c) 2004-5 by martin f. krafft <madduck@debian.org>
+# deb-reversion copyright 2004-5 by martin f. krafft <madduck@debian.org>
 # contributors: Goswin von Brederlow, Filippo Giunchedi
+# Modifications copyright 2006 by Julian Gilbey <jdg@debian.org>
 #
 # TODO: 
 #   - add debugging output.
@@ -16,20 +17,20 @@
 #   - allow calculation of specified version
 #
 
-PROGNAME=${0##*/}
-PROGVERSION=0.1.19
+PROGNAME=`basename $0`
 
 blurb()
 {
-  cat <<EOF
-$PROGNAME version $PROGVERSION -- a script to bump a .deb file's version number.
+    cat <<EOF
+$PROGNAME -- a script to bump a .deb file's version number.
 EOF
 }
 
 copyright()
 {
-  cat <<EOF
-$PROGNAME is (c) 2004-5 by martin f. krafft <madduck@debian.org>.
+    cat <<EOF
+$PROGNAME is Copyright 2004-5 by martin f. krafft <madduck@debian.org>.
+Modifications are Copyright 2006 by Julian Gilbey <jdg@debian.org>.
 This programme is part of devscripts ###VERSION###.
 
 The programme has been published under the terms of the Artistic Licence.
@@ -41,14 +42,14 @@ EOF
 
 about()
 {
-  blurb
-  echo
-  copyright
+    blurb
+    echo
+    copyright
 } 
 
 usage()
 {
-  cat <<EOF
+    cat <<EOF
 Usage:
   $PROGNAME [options] file [log message]
 
@@ -82,137 +83,137 @@ TEMP=$(getopt -s bash -o $SHORTOPTS -l $LONGOPTS --n $PROGNAME -- "$@")
 eval set -- $TEMP
 
 while [ $# -gt 0 ]; do
-  opt=$1
-  shift
+    opt=$1
+    shift
 
-  if [ "$mode" = opts ]; then
-    case $opt in
-      -*) unset OPT_STATE;;
-      *)
-        case $OPT_STATE in
-          SET_VERSION)
-            VERSION=$opt
-            continue;;
-          SET_HOOK)
-            HOOK=$opt
-            continue;;
-        esac
-    esac
-    
-    case $opt in
-      -h|--help) usage >&2; exit 0;;
-      -v|--new-version) OPT_STATE=SET_VERSION;;
-      -c|--calculate-only) CALCULATE=1;;
-      -k|--hook) OPT_STATE=SET_HOOK;;
-      -D|--debug) DPKGDEB_DEBUG=--debug;;
-      -V|--version) about >&2; exit 0;;
-      --) mode=debfile; continue;;
-    esac
-  elif [ "$mode" = debfile ]; then
-    case $opt in
-      /*.deb) DEB=$opt;;
-       *.deb) DEB=${DIR}/$opt;;
-       *) echo "$PROGNAME error: no .deb file has been specified." >&2
-          usage
-          exit -1;;
-    esac
-    mode=log
-  else
-    LOG="${LOG:+$LOG }$opt"
-  fi    
+    if [ "$mode" = opts ]; then
+	case $opt in
+	    -*) unset OPT_STATE;;
+	    *)
+		case $OPT_STATE in
+		    SET_VERSION)
+			VERSION=$opt
+			continue;;
+		    SET_HOOK)
+			HOOK=$opt
+			continue;;
+		esac
+	esac
+	
+	case $opt in
+	    -h|--help) usage >&2; exit 0;;
+	    -v|--new-version) OPT_STATE=SET_VERSION;;
+	    -c|--calculate-only) CALCULATE=1;;
+	    -k|--hook) OPT_STATE=SET_HOOK;;
+	    -D|--debug) DPKGDEB_DEBUG=--debug;;
+	    -V|--version) about >&2; exit 0;;
+	    --) mode=debfile; continue;;
+	esac
+    elif [ "$mode" = debfile ]; then
+	case $opt in
+	    /*.deb) DEB=$opt;;
+	    *.deb) DEB=${DIR}/$opt;;
+	    *)  echo "$PROGNAME error: no .deb file has been specified." >&2
+		usage
+		exit -1;;
+	esac
+	mode=log
+    else
+	LOG="${LOG:+$LOG }$opt"
+    fi    
 done
 
 
 if [ -z "$DEB" ]; then
-  echo "$PROGNAME error: no .deb file has been specified." >&2
-  usage
-  exit -1
+    echo "$PROGNAME error: no .deb file has been specified." >&2
+    usage
+    exit -1
 elif [ ! -f $DEB ]; then
-  echo "$PROGNAME error: $.deb does not exist." >&2
-  exit -2
+    echo "$PROGNAME error: $.deb does not exist." >&2
+    exit -2
 fi
 
 if [ -n "$VERSION" -a -n "$CALCULATE" ]; then
-  echo "$PROGNAME error: the options -v and -c cannot be used together" >&2
-  usage
-  exit -1
+    echo "$PROGNAME error: the options -v and -c cannot be used together" >&2
+    usage
+    exit -1
 fi
 
 make_temp_dir()
 {
-  TMPDIR=$(mktemp -d /tmp/deb-reversion.XXXXXX)
-  trap "rm -rf $TMPDIR" 0
-  mkdir -p ${TMPDIR}/package
-  TMPDIR=${TMPDIR}/package
+    TMPDIR=$(mktemp -d /tmp/deb-reversion.XXXXXX)
+    trap "rm -rf $TMPDIR" 0
+    mkdir -p ${TMPDIR}/package
+    TMPDIR=${TMPDIR}/package
 }
 
 extract_deb_file()
 {
-  dpkg-deb $DPKGDEB_DEBUG --extract $1 .
-  dpkg-deb $DPKGDEB_DEBUG --control $1 DEBIAN
+    dpkg-deb $DPKGDEB_DEBUG --extract $1 .
+    dpkg-deb $DPKGDEB_DEBUG --control $1 DEBIAN
 }
 
 get_version()
 {
-  dpkg --info $1 | sed -ne 's,^[[:space:]]Version: ,,p'
+    dpkg --info $1 | sed -ne 's,^[[:space:]]Version: ,,p'
 }
 
 bump_version()
 {
-  VERSTR='+0.local.'
-  case $1 in
-    *${VERSTR}[0-9]*)
-      REV=${1##*${VERSTR}}
-      echo ${1%${VERSTR}*}${VERSTR}$((++REV));;
-    *-*)
-      echo ${1}${VERSTR}1;;
-    *) 
-      echo ${1}-0${VERSTR}1;;
-  esac
+    VERSTR='+0.local.'
+    case $1 in
+	*${VERSTR}[0-9]*)
+	    REV=${1##*${VERSTR}}
+	    echo ${1%${VERSTR}*}${VERSTR}$((++REV));;
+	*-*)
+	    echo ${1}${VERSTR}1;;
+	*) 
+	echo ${1}-0${VERSTR}1;;
+    esac
 }
 
 call_hook()
 {
-  export VERSION
-  sh -c "$HOOK"
+    export VERSION
+    sh -c "$HOOK"
 }
 
 change_version()
 {
-  PACKAGE=$(sed -ne 's,^Package: ,,p' DEBIAN/control)
-  VERSION=$1
-  for i in changelog{,.Debian}.gz; do
-    [ -f usr/share/doc/${PACKAGE}/$i ] \
-      && LOGFILE=usr/share/doc/${PACKAGE}/$i
-  done
-  [ -z "$LOGFILE" ] && return 1
-  mkdir -p debian
-  zcat $LOGFILE > debian/changelog
-  shift
-  dch -v $VERSION -- $@
-  call_hook
-  gzip -9 -c debian/changelog >| $LOGFILE
-  sed -i -e "s,^Version: .*,Version: $VERSION," DEBIAN/control
-  rm -rf debian
+    PACKAGE=$(sed -ne 's,^Package: ,,p' DEBIAN/control)
+    VERSION=$1
+    for i in changelog{,.Debian}.gz; do
+	[ -f usr/share/doc/${PACKAGE}/$i ] \
+	    && LOGFILE=usr/share/doc/${PACKAGE}/$i
+    done
+    [ -z "$LOGFILE" ] && return 1
+    mkdir -p debian
+    zcat $LOGFILE > debian/changelog
+    shift
+    dch -v $VERSION -- $@
+    call_hook
+    gzip -9 -c debian/changelog >| $LOGFILE
+    sed -i -e "s,^Version: .*,Version: $VERSION," DEBIAN/control
+    rm -rf debian
 }
 
 repack_file()
 {
-  cd ..
-  dpkg-deb -b package >/dev/null
-  dpkg-name package.deb | sed -e 's,.*to `\(.*\).,\1,'
+    cd ..
+    dpkg-deb -b package >/dev/null
+    dpkg-name package.deb | sed -e 's,.*to `\(.*\).,\1,'
 }
 
 [ -z "$VERSION" ] && VERSION=$(bump_version $(get_version $DEB))
 
 if [ -n "$CALCULATE" ]; then
-  echo $VERSION
-  exit 0
+    echo $VERSION
+    exit 0
 fi
 
 if [ `id -u` -ne 0 ]; then
-  echo "$PROGNAME error: need to run me as root or using fakeroot!" >&2
-  exit -3
+    echo "$PROGNAME error: need to run me as root or using fakeroot!" >&2
+    exit -3
 fi
 
 make_temp_dir
