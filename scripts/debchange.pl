a339c8f9ecd9cca134cd05a2440f0204aaebbd10
Luk Claes <luk@debian.org>
2007-04-18 17:07:05 +0000

    Use /etc/timezone if $TZ is not set.

--- scripts/debchange.pl
+++ scripts/debchange.pl
@@ -183,6 +183,10 @@ if (@ARGV and $ARGV[0] =~ /^--no-?conf$/) {
 		       'DEBCHANGE_MULTIMAINT' => 'yes',
 		       'DEBCHANGE_TZ' => $ENV{TZ}, # undef if TZ unset
 		       );
+     if (!$ENV{TZ} && open TIMEZONE, "/etc/timezone") {
+     	chomp(my $timezone = <TIMEZONE>);
+     	$config_vars{'DEBCHANGE_TZ'} = $timezone;
+     }
     my %config_default = %config_vars;
     
     my $shell_cmd;
