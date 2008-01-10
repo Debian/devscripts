ae86ac0b67041c27eb8ee634cada28e96effd6d1
Luk Claes <luk@debian.org>
2008-01-10 18:10:00 +0000

    * whodepends: Support showing source packages instead of binary ones thanks   to Otavio Salvador.

--- scripts/whodepends.sh
+++ scripts/whodepends.sh
@@ -11,6 +11,7 @@ usage () {
 Usage: $PROGNAME [package] [package] ... [options]
   Check which maintainers a particular package depends on.
   $PROGNAME options:
+    --source, -s      Show source packages instead of binary ones.
     --help, -h        Show this help screen.
     --version         Show version and copyright information.
 EOF
@@ -30,14 +31,21 @@ fi
 
 while [ -n "$1" ]; do
 	case "$1" in
+		-s | --source) source=true ;;
 		-h | --help) usage; exit 0 ;;
 		--version) version; exit 0 ;;
 		*)
 			echo "Dependent maintainers for $1:"
 			for package in `apt-cache showpkg $1 | sed -n '/Reverse Depends:/,/Dependencies/p' | grep '^ '|sed 's/,.*//'`; do
-				apt-cache show $package |
+				if [ $source ]; then
+					apt-cache showsrc $package |
+					awk '/^Maintainer:/ {maint=$0} /^Package:/ {pkg=$0} END {print maint, pkg}' |
+					sed 's/Maintainer: //;s/Package: //'
+				else
+					apt-cache show $package |
 					awk '/^Maintainer:/ {maint=$0} END {print maint, "'$package'"}' |
 					sed 's/Maintainer: //'
+				fi
 			done | sort -u | awk -F'>' '{ pack[$1]=pack[$1] $2 } END {for (val in pack) print val ">", "(" pack[val] ")"}' | sed 's/( /(/'
 			echo
 		;;
