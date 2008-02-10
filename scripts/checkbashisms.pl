be71ab5f13380d9a272e3d66ddfca9622ff0ad9f
Adam D. Barratt <adam@adam-barratt.org.uk>
2008-02-10 19:52:17 +0000

    Tweak the "source passing arguments" check again...

--- scripts/checkbashisms.pl
+++ scripts/checkbashisms.pl
@@ -165,7 +165,7 @@ foreach my $filename (@ARGV) {
 	    }
 	    # since this test is ugly, I have to do it by itself
 	    # detect source (.) trying to pass args to the command it runs
-	    if (not $found and m/^\s*(\.\s+[^\s]+\s+([^\s]+))/) {
+	    if (not $found and m/^\s*(\.\s+[^\s;]+\s+([^\s]+))/) {
 		if ($2 =~ /^(\&|\||\d?>|<|[^`]+`|;)/) {
 		    # everything is ok
 		    ;
