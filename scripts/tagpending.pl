27facf941745620f2e8a90aa0bb09a77c51087e4
Adam D. Barratt <adam@adam-barratt.org.uk>
2008-02-14 19:37:43 +0000

    * tagpending: Rewrite in perl using Debbugs.pm * README, debian/control: Update description of tagpending

---
+++ scripts/tagpending.pl
@@ -0,0 +1,336 @@
+#! /usr/bin/perl -w
+#
+# tagpending: Parse a Debian changelog for a list of bugs closed
+# and tag any that are not already pending as such.
+#
+# The original shell version of tagpending was written by Joshua Kwan
+# and is Copyright 2004 Joshua Kwan <joshk@triplehelix.org>
+# with changes copyright 2004-07 by their respective authors.
+#
+# This version is
+#   Copyright 2008 Adam D. Barratt
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Basename;
+use lib '/usr/share/devscripts';
+use Devscripts::Debbugs;
+
+sub bugs_info;
+
+my $progname = basename($0);
+
+my ($opt_help, $opt_version, $opt_verbose, $opt_noact, $opt_silent);
+my ($opt_online, $opt_confirm, $opt_to, $opt_wnpp);
+
+# Default options
+$opt_silent = 0;
+$opt_verbose = 0;
+$opt_online = 1;
+$opt_noact = 0;
+$opt_confirm = 0;
+$opt_wnpp = 0;
+$opt_to = '';
+
+GetOptions("help|h" => \$opt_help,
+	   "version" => \$opt_version,
+	   "verbose|v!" => \$opt_verbose,
+	   "noact|n" => \$opt_noact,
+	   "silent|s" => \$opt_silent,
+	   "force|f" => sub { $opt_online = 0; },
+	   "confirm|c" => \$opt_confirm,
+	   "to|t=s" => \$opt_to,
+	   "wnpp|w" => \$opt_wnpp,
+           )
+    or die "Usage: $progname [options]\nRun $progname --help for more details\n";
+
+$opt_to = "-v$opt_to" if $opt_to;
+
+if ($opt_help) {
+    help(); exit 0;
+} elsif ($opt_version) {
+    version(); exit 0;
+}
+
+if ($opt_verbose and $opt_silent) {
+    die "$0 error: --silent and --verbose contradict each other\n";
+}
+
+=head1 SYNOPSIS
+
+B<tagpending> [options]
+
+=head1 DESCRIPTION
+
+B<tagpending> parses debian/changelog and debian/control to determine 
+which bugs would be closed if the package were uploaded. Each bug is 
+then marked as pending, using B<bts>(1) if it is not already so.
+
+=head1 OPTIONS
+
+=over 4
+
+=item -n, --noact
+
+Check whether any bugs require tagging, but do not actually do so.
+
+=item -s, --silent
+
+Do not output any messages.
+
+=item -v, --verbose
+
+List each bug checked and tagged in turn.
+
+=item -f, --force
+
+Do not query the BTS, but (re)tag all bugs closed in the changelog.
+
+=item -c, --confirm
+
+Tag bugs as both confirmed and pending.
+
+=item -t, --to <version>
+
+Parse changelogs for all versions strictly greater than <version>.
+
+Equivalent to dpkg-parsechangelog's -v option.
+
+=item -w, --wnpp
+
+For each bug that does not appear to belong to the current package, 
+check whether it is filed against wnpp. If so, tag it. This allows e.g.
+ITAs and ITPs closed in an upload to be tagged.
+
+=back
+
+=head1 SEE ALSO
+
+B<bts>(1) and B<dpkg-parsechangelog>(1)
+
+=cut
+
+my $source;
+my @closes;
+
+foreach my $file ("debian/control", "debian/changelog") {
+    if (! -f $file) {
+	die "$progname error: $file does not exist!\n";
+    }
+}
+
+open PARSED, "dpkg-parsechangelog $opt_to |";
+
+while (<PARSED>) {
+    if (/^Source: (.*)/) {
+	$source = $1;
+    } elsif (/^Closes: (.*)$/) {
+	@closes = split ' ', $1;
+    }
+}
+
+close PARSED;
+
+my $pending;
+my $open;
+
+if ($opt_online) {
+    $pending = Devscripts::Debbugs::select( "src:$source", "status:open", "status:forwarded", "tag:pending" );
+    $open = Devscripts::Debbugs::select( "src:$source", "status:open", "status:forwarded" );
+}
+
+my %bugs = map { $_ => 1} @closes;
+if ($pending) {
+    %bugs = ( %bugs, map { $_ => 1} @{$pending} );
+}
+
+my $bug;
+my $message;
+my @to_tag = ();
+my @wnpp_to_tag = ();
+
+foreach $bug (keys %bugs) {
+    print "Checking bug #$bug: " if $opt_verbose;
+
+    if (grep /^$bug$/, @{$pending}) {
+	print "already marked pending\n" if $opt_verbose;
+    } else {
+	if (grep /^$bug$/, @{$open} or not $opt_online) {
+	    print "needs tag\n" if $opt_verbose;
+	    push (@to_tag, $bug);
+	} else {
+	    if ($opt_wnpp) {
+		my $status = Devscripts::Debbugs::status($bug);
+		if ($status->{$bug}->{package} eq 'wnpp') {
+		    if (!$status->{$bug}->{tags} =~ /pending/) {
+			print "wnpp needs tag\n" if $opt_verbose;
+			push (@wnpp_to_tag, $bug);
+		    } else {
+			print "wnpp already marked pending\n" if $opt_verbose;
+		    }
+		} else {
+		    $message = "is closed or does not belong to this package (check bug # or force)\n";
+
+		    print "Warning: #$bug " if not $opt_verbose;
+		    print "$message";
+		}
+	    } else {
+		$message = "is closed or does not belong to this package (check bug # or force)\n";
+
+		print "Warning: #$bug " if not $opt_verbose;
+		print "$message";
+	    }
+	}
+    }
+}
+
+if (!@to_tag and !@wnpp_to_tag) {
+    print "$progname info: Nothing to do, exiting.\n"
+	if $opt_verbose or !$opt_silent;
+    exit 0;
+}
+
+my @sourcepkgs = ();
+if (@to_tag) {
+    open CONTROL, "debian/control";
+
+    while (<CONTROL>) {
+	if (/^(Package|Source): (.*)/) {
+	    push (@sourcepkgs, $2);
+	}
+    }
+
+    close CONTROL;
+}
+
+my %packages = map { $_ => 1 } @sourcepkgs;
+my @bts_args = ("bts");
+
+if ($opt_noact) {
+    bugs_info;
+    bugs_info "wnpp" if $opt_wnpp;
+} else {
+    if (!$opt_silent) {
+	bugs_info;
+	bugs_info "wnpp" if $opt_wnpp;
+    }
+
+    if (@to_tag) {
+	push(@bts_args, "package", join " ", keys(%packages));
+
+	foreach my $bug (@to_tag) {
+	    push(@bts_args, ".", "tag", $bug, "+ pending");
+	    push(@bts_args, "confirmed") if $opt_confirm;
+	}
+    }
+    if (@wnpp_to_tag) {
+	push(@bts_args, ".") if scalar @bts_args > 1;
+	push(@bts_args, "package wnpp");
+	foreach my $wnpp_bug (@wnpp_to_tag) {
+	    push(@bts_args, ".", "tag", $wnpp_bug, "+ pending");
+	}
+    }
+
+    system @bts_args;
+}
+
+sub bugs_info {
+    my $type = shift || '';
+    my @bugs;
+
+    if ($type eq "wnpp") {
+	if (@wnpp_to_tag) {
+	    @bugs = @wnpp_to_tag;
+	} else {
+	    return;
+	}
+    } else {
+	@bugs = @to_tag;
+    }
+
+    print "$progname info: ";
+
+    if ($opt_noact) {
+	print "would tag";
+    } else {
+	print "tagging";
+    }
+
+    print " these";
+    print " wnpp" if $type eq "wnpp";
+    print " bugs pending";
+    print " and confirmed" if $opt_confirm and $type ne "wnpp";
+    print ":";
+
+    foreach my $bug (@bugs) {
+	print " $bug";
+    }
+
+    print "\n";
+}
+
+sub help {
+   print <<"EOF";
+Usage: $progname [options]
+
+Valid options are:
+   --help, -h           Display this message
+   --version            Display version and copyright info
+    -n, --noact         Only simulate what would happen during this run;
+			do not tag any bugs.
+    -s, --silent        Silent mode
+    -v, --verbose       Verbose mode: List bugs checked/tagged.
+                        NOTE: Verbose and silent mode can't be used together.
+    -f, --force         Do not query the BTS; (re-)tag all bug reports.
+    -c, --confirm       Tag bugs as confirmed as well as pending
+    -t, --to <version>  Use changelog information from all versions strictly
+			later than <version> (mimics dpkg-parsechangelog's
+			-v option.)
+    -w, --wnpp          For each potentially not owned bug, check whether
+			it is filed against wnpp and, if so, tag it. This
+			allows e.g. ITA or ITPs to be tagged.
+
+EOF
+}
+
+sub version {
+    print <<"EOF";
+This is $progname, from the Debian devscripts package, version ###VERSION###
+Copyright 2008 by Adam D. Barratt <adam\@adam-barratt.org.uk>; based
+on the shell script by Joshua Kwan <joshk\@triplehelix.org>.
+
+This program comes with ABSOLUTELY NO WARRANTY.
+You are free to redistribute this code under the terms of the
+GNU General Public License, version 2, or (at your option) any
+later version.
+EOF
+}
+
+=head1 COPYRIGHT
+
+This program is Copyright 2008 by Adam D. Barratt 
+<adam@adam-barratt.org.uk>.
+
+The shell script tagpending, on which this program is based, is 
+Copyright 2004 by Joshua Kwan <joshk@triplehelix.org> with changes
+copyright 2004-7 by their respective authors.
+
+This program is licensed under the terms of the GPL, either version 2 of 
+the License, or (at your option) any later version.
+
+=cut
