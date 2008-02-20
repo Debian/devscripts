6ffceef426fde9f9db4ef38c4fbb699786e52316
Patrick Schoenfeld <schoenfeld@in-medias-res.com>
2008-02-20 10:24:23 +0000

    * licensecheck: Support a less common form of MIT license usage

--- scripts/licensecheck.pl
+++ scripts/licensecheck.pl
@@ -383,6 +383,10 @@ sub parselicense($) {
 	$license = "QPL $license";
     }
 
+	if ($licensetext =~ /http:\/\/opensource.org\/licenses\/mit-license.php/) {
+	$license = "MIT/X11 (BSD like) $license";
+	}
+
     if ($licensetext =~ /Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files \(the Software\), to deal in the Software/) {
 	$license = "MIT/X11 (BSD like) $license";
     }
