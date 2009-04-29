bb15e9db6543eb03c454d7c5e862464da037752a
Adam D. Barratt <adsb@debian.org>
2009-04-29 16:39:22 +0000

    namecheck: Fix the formatting of a number of POD strings, also making them easier to translate.  Thanks, Florentin Duneau.  (Closes: #526047)

--- scripts/namecheck.pl
+++ scripts/namecheck.pl
@@ -4,56 +4,40 @@
 
 namecheck - Check project names are not already taken.
 
-=cut
-
 =head1 ABOUT
 
-  This is a simple tool to automate the testing of project
- names at the most common Open Source / Free Software hosting
- environments.
+This is a simple tool to automate the testing of project names at the most
+common Open Source / Free Software hosting environments.
 
-  Each new project requires a name, and those names are ideally
- unique.  To come up with names is hard, and testing to ensure
- they're not already in use is time-consuming - unless you have
- a tool such as this one.
-
-=cut
+Each new project requires a name, and those names are ideally unique.  To come
+up with names is hard, and testing to ensure they're not already in use is
+time-consuming - unless you have a tool such as this one.
 
 =head1 CUSTOMIZATION
 
-  The script, as-is, contains a list of sites, and patterns, to
- test against.
+The script, as-is, contains a list of sites, and patterns, to test against.
 
-  If those patterns aren't sufficient then you may create your own
- additions and add them to the script.  If you wish to have your
- own version of the patterns you may save them into the file
- ~/.namecheckrc
-
-=cut
-
+If those patterns aren't sufficient then you may create your own additions and
+add them to the script.  If you wish to have your own version of the patterns
+you may save them into the file ~/.namecheckrc
 
 =head1 HOMEPAGE
 
-  The most recent version of this script may be found here:
+The most recent version of this script may be found here:
 
-    http://mybin.repository.steve.org.uk/?raw-file/tip/namecheck
-
-=cut
+http://mybin.repository.steve.org.uk/?raw-file/tip/namecheck
 
 =head1 AUTHOR
 
- Steve
- --
- http://www.steve.org.uk/
-
-=cut
+Steve
+--
+http://www.steve.org.uk/
 
 =head1 LICENSE
 
 Copyright (c) 2008 by Steve Kemp.  All rights reserved.
 
-This module is free software;
-you can redistribute it and/or modify it under
+This module is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
 
 =cut
