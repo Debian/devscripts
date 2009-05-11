89d30fa0885374ff9f4bb43f6ae2dd0dd77389b7
Adam D. Barratt <adsb@debian.org>
2009-05-11 19:51:53 +0000

    uscan: Pass -n to gzip when repacking bz2 files to ensure consistent checksums.  Thanks, Damyan Ivanov.  (Closes: #504672)

--- scripts/uscan.pl
+++ scripts/uscan.pl
@@ -1289,7 +1289,7 @@ EOF
     if ($repack and $newfile_base =~ /^(.*)\.(tar\.bz2|tbz2?)$/) {
 	print "-- Repacking from bzip2 to gzip\n" if $verbose;
 	my $newfile_base_gz = "$1.tar.gz";
-	system("bunzip2 -c $destdir/$newfile_base | gzip -9 > $destdir/$newfile_base_gz") == 0
+	system("bunzip2 -c $destdir/$newfile_base | gzip -n -9 > $destdir/$newfile_base_gz") == 0
 	  or die "repacking from bzip2 to gzip failed\n";
 	unlink "$destdir/$newfile_base";
 	$newfile_base = $newfile_base_gz;
