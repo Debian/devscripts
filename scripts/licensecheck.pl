60a74e3492cd1c777276bd839bf38668215cc8e8
Adam D. Barratt <adsb@debian.org>
2009-05-29 17:11:23 +0000

    licensecheck: Fix an error in a regular expression which led to an uninitialized value warning and missing version for some GPL headers. Thanks, Jakub Wilk.  (Closes: #531059)

--- scripts/licensecheck.pl
+++ scripts/licensecheck.pl
@@ -387,7 +387,7 @@ sub parselicense($) {
     my $license = "";
 
     if ($licensetext =~ /version ([^ ]+) (?:\(?only\)?.? )?(?:of the GNU (Affero )?General Public License )?as published by the Free Software Foundation/i or
-	$licensetext =~ /GNU (Affero )?General Public License as published by the Free Software Foundation; version ([^ ]+) /i) {
+	$licensetext =~ /GNU (?:Affero )?General Public License as published by the Free Software Foundation; version ([^ ]+) /i) {
 
 	$gplver = " (v$1)";
     } elsif ($licensetext =~ /GNU (Affero ?)General Public License, version ([^ ]+?)[ .]/) {
