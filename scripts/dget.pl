e48a8c9349bb74a3d3137da6f4eb7f9db6972e27
James Vega <jamessan@debian.org>
2010-09-04 15:14:39 -0400

    dget: Fix parsing of apt-cache and sources.list when given only a package name.
    Closes: #594904
    Signed-off-by: James Vega <jamessan@debian.org>

--- scripts/dget.pl
+++ scripts/dget.pl
@@ -262,7 +262,9 @@ sub apt_get {
 	if ($qversion and /^ [ *]{3} ($qversion) 0/) {
 	    while (<$apt>) {
 		last OUTER unless /^  *(?:\d+) (\S+)/;
-		push @hosts, $1;
+		(my $host = $1) =~ s@/$@@;
+		next if $host eq '/var/lib/dpkg/status';
+		push @hosts, $host;
 	    }
 	}
     }
@@ -274,7 +276,7 @@ sub apt_get {
 	die "$progname: no hostnames in apt-cache policy $package for $version found\n";
     }
 
-    $apt = new IO::File("LC_ALL=C apt-cache --all-versions show $package |")
+    $apt = new IO::File("LC_ALL=C apt-cache show $package=$version |")
 	or die "$!";
     my ($v, $p, $filename, $md5sum);
     while (<$apt>) {
@@ -312,7 +314,7 @@ sub apt_get {
     if (-f "/etc/apt/sources.list") {
 	$apt = new IO::File("/etc/apt/sources.list") or die "/etc/apt/sources.list: $!";
 	while (<$apt>) {
-	    if (/^\s*deb\s*($host_re\S+)/) {
+	    if (/^\s*deb\s*($host_re\b)/) {
 		push @repositories, $1;
 	    }
 	}
@@ -325,7 +327,7 @@ sub apt_get {
 	$_ = "/etc/apt/sources.list.d/$_";
 	$apt = new IO::File("$_") or die "$_: $!";
 	while (<$apt>) {
-	    if (/^\s*deb\s*($host_re\S+)/) {
+	    if (/^\s*deb\s*($host_re\b)/) {
 		push @repositories, $1;
 	    }
 	}
