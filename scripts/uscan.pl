b3df3f93fcdd59c9de49c7c53461ab558ac2f5b4
James Vega <jamessan@debian.org>
2011-04-10 23:15:20 -0400

    Add changelog entry for Christian's uscan patch and use glob instead of ls.
    Signed-off-by: James Vega <jamessan@debian.org>

--- scripts/uscan.pl
+++ scripts/uscan.pl
@@ -1373,10 +1373,11 @@ EOF
 	my $newfile_base_gz = "$1.tar.gz";
 	my $tempdir = tempdir ( "uscanXXXX", TMPDIR => 1, CLEANUP => 1 );
 	my $globpattern = "*";
+	my $hidden = ".[!.]*";
 	system("unzip -q -a -d $tempdir $destdir/$newfile_base") == 0
 	  or die("Repacking from zip to tar.gz failed (could not unzip)\n");
-	if (system("ls -d $tempdir/.[!.]* >/dev/null 2>&1") == 0) {
-	    $globpattern .= " .[!.]*";
+	if (defined glob($hidden)) {
+	    $globpattern .= " $hidden";
 	}
 	system("cd $tempdir; GZIP=-9 tar --owner=root --group=root --mode=a+rX -czf $destdir/$newfile_base_gz $globpattern") == 0
 	  or die("Repacking from zip to tar.gz failed (could not create tarball)\n");
