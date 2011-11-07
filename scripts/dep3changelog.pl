be72de2b9ba8c8e7be782f5c2c259341d1a6e235
James McCoy <jamessan@debian.org>
2011-11-06 21:46:12 -0500

    Reformat dep3changelog and add it to the list in README
    Signed-off-by: James McCoy <jamessan@debian.org>

--- scripts/dep3changelog.pl
+++ scripts/dep3changelog.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/perl -w
+#! /usr/bin/perl
 
 # dep3changelog: extract a DEP3 patch header from the named file and
 # automatically update debian/changelog with a suitable entry
@@ -22,15 +22,13 @@
 
 use 5.008;  # We're using PerlIO layers
 use strict;
+use warnings;
 use open ':utf8';  # patch headers are required to be UTF-8
 
 # for checking whether user names are valid and making format() behave
 use Encode qw/decode_utf8 encode_utf8/;
 use Getopt::Long;
 use File::Basename;
-
-# Predeclare functions
-my $warnings = 0;
 
 # And global variables
 my $progname = basename($0);
@@ -60,7 +58,7 @@ EOF
 
 my ($opt_help, $opt_version);
 GetOptions("help|h" => \$opt_help,
-	"version" => \$opt_version,
+	   "version" => \$opt_version,
 )
 or die "Usage: $progname patch [... patch] [-- [dch options]]\nRun $progname --help for more details\n";
 
@@ -70,7 +68,7 @@ if ($opt_version) { version; exit 0; }
 my @patches;
 
 while (@ARGV && $ARGV[0] !~ /^-/) {
-	push(@patches,shift(@ARGV));
+    push(@patches,shift(@ARGV));
 }
 
 # Check, sanitise and decode these environment variables
@@ -80,96 +78,95 @@ check_env_utf8('DEBEMAIL');
 check_env_utf8('EMAIL');
 
 if (exists $env{'DEBEMAIL'} and $env{'DEBEMAIL'} =~ /^(.*)\s+<(.*)>$/) {
-	$env{'DEBFULLNAME'} = $1 unless exists $env{'DEBFULLNAME'};
-	$env{'DEBEMAIL'} = $2;
+    $env{'DEBFULLNAME'} = $1 unless exists $env{'DEBFULLNAME'};
+    $env{'DEBEMAIL'} = $2;
 }
 if (! exists $env{'DEBEMAIL'} or ! exists $env{'DEBFULLNAME'}) {
-	if (exists $env{'EMAIL'} and $env{'EMAIL'} =~ /^(.*)\s+<(.*)>$/) {
-		$env{'DEBFULLNAME'} = $1 unless exists $env{'DEBFULLNAME'};
-		$env{'EMAIL'} = $2;
-	}
+    if (exists $env{'EMAIL'} and $env{'EMAIL'} =~ /^(.*)\s+<(.*)>$/) {
+	$env{'DEBFULLNAME'} = $1 unless exists $env{'DEBFULLNAME'};
+	$env{'EMAIL'} = $2;
+    }
 }
 
 my $fullname = '';
 my $email = '';
 
 if (exists $env{'DEBFULLNAME'}) {
-	$fullname = $env{'DEBFULLNAME'};
+    $fullname = $env{'DEBFULLNAME'};
 } elsif (exists $env{'NAME'}) {
-	$fullname = $env{'NAME'};
+    $fullname = $env{'NAME'};
 } else {
-	my @pw = getpwuid $<;
-	if ($pw[6]) {
-		if (my $pw = decode_utf8($pw[6])) {
-			$pw =~ s/,.*//;
-			$fullname = $pw;
-		} else {
-			warn "$progname warning: passwd full name field for uid $<\nis not UTF-8 encoded; ignoring\n";
-			$warnings++;
-		}
+    my @pw = getpwuid $<;
+    if ($pw[6]) {
+	if (my $pw = decode_utf8($pw[6])) {
+	    $pw =~ s/,.*//;
+	    $fullname = $pw;
+	} else {
+	    warn "$progname warning: passwd full name field for uid $<\nis not UTF-8 encoded; ignoring\n";
 	}
+    }
 }
 
 if (exists $env{'DEBEMAIL'}) {
-	$email = $env{'DEBEMAIL'};
+    $email = $env{'DEBEMAIL'};
 } elsif (exists $env{'EMAIL'}) {
-	$email = $env{'EMAIL'};
+    $email = $env{'EMAIL'};
 }
 
 for my $patch (@patches) {
-	my $shebang = 0;
-	my $dpatch = 0;
-	# TODO: more than one debian or launchpad bug in a patch?
-	my ($description,$author,$debbug,$lpbug,$origin);
+    my $shebang = 0;
+    my $dpatch = 0;
+    # TODO: more than one debian or launchpad bug in a patch?
+    my ($description,$author,$debbug,$lpbug,$origin);
 
-	next unless (open PATCH, $patch);
-	while (<PATCH>) {
-		# first line only
-		if (!$shebang) {
-			$shebang = 1;
-			if (/^#!/) {
-				$dpatch = $shebang = 1;
-				next;
-			}
-		}
-		last if (/^---/ || /^\s*$/);
-		chomp;
-		# only if there was a shebang do we strip comment chars
-		s/^# // if ($dpatch);
-		# fixme: this should only apply to the description field.
-		next if (/^ /);
-
-		if (/^(Description|Subject):\s+(.*)\s*/) {
-			$description = $2;
-		} elsif (/^(Author|From):\s+(.*)\s*/) {
-			$author = $2;
-		} elsif (/^Origin:\s+(.*)\s*/) {
-			$origin = $1;
-		} elsif (/^bug-debian:\s+http:\/\/bugs\.debian\.org\/([0-9]+)\s*/i) {
-			$debbug = $1;
-		} elsif (/^bug-ubuntu:\s+https:\/\/.*launchpad\.net\/.*\/([0-9]+)\s*/i) {
-			$lpbug = $1;
-		}
-	}
-	close PATCH;
-	if (!$description || (!$origin && !$author)) {
-		print STDERR "$patch: Invalid DEP3 header\n";
+    next unless (open PATCH, $patch);
+    while (<PATCH>) {
+	# first line only
+	if (!$shebang) {
+	    $shebang = 1;
+	    if (/^#!/) {
+		$dpatch = $shebang = 1;
 		next;
+	    }
 	}
-	my $changelog = "$patch: $description";
-	$changelog .= '.' unless ($changelog =~ /\.$/);
-	if ($author && $author ne $fullname && $author ne "$fullname <$email>")
-	{
-		$changelog .= "  Thanks to $author.";
+	last if (/^---/ || /^\s*$/);
+	chomp;
+	# only if there was a shebang do we strip comment chars
+	s/^# // if ($dpatch);
+	# fixme: this should only apply to the description field.
+	next if (/^ /);
+
+	if (/^(Description|Subject):\s+(.*)\s*/) {
+	    $description = $2;
+	} elsif (/^(Author|From):\s+(.*)\s*/) {
+	    $author = $2;
+	} elsif (/^Origin:\s+(.*)\s*/) {
+	    $origin = $1;
+	} elsif (/^bug-debian:\s+http:\/\/bugs\.debian\.org\/([0-9]+)\s*/i) {
+	    $debbug = $1;
+	} elsif (/^bug-ubuntu:\s+https:\/\/.*launchpad\.net\/.*\/([0-9]+)\s*/i) {
+	    $lpbug = $1;
 	}
-	if ($debbug || $lpbug) {
-		$changelog .= '  Closes';
-		$changelog .= ": #$debbug" if ($debbug);
-		$changelog .= "," if ($debbug && $lpbug);
-		$changelog .= " LP: #$lpbug" if ($lpbug);
-		$changelog .= '.';
-	}
-	system('dch',$changelog,@ARGV);
+    }
+    close PATCH;
+    if (!$description || (!$origin && !$author)) {
+	warn "$patch: Invalid DEP3 header\n";
+	next;
+    }
+    my $changelog = "$patch: $description";
+    $changelog .= '.' unless ($changelog =~ /\.$/);
+    if ($author && $author ne $fullname && $author ne "$fullname <$email>")
+    {
+	$changelog .= "  Thanks to $author.";
+    }
+    if ($debbug || $lpbug) {
+	$changelog .= '  Closes';
+	$changelog .= ": #$debbug" if ($debbug);
+	$changelog .= "," if ($debbug && $lpbug);
+	$changelog .= " LP: #$lpbug" if ($lpbug);
+	$changelog .= '.';
+    }
+    system('dch',$changelog,@ARGV);
 }
 
 # Is the environment variable valid or not?
