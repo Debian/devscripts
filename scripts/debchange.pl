5a4c6cf51c2e0fdf8bc1ba00df2e4c6be7b16ffe
Benjamin Drung <bdrung@debian.org>
2012-07-07 00:33:25 +0200

    debchange: Recognize stable-proposed-updates as Debian distribution.
    Closes: #677613

--- scripts/debchange.pl
+++ scripts/debchange.pl
@@ -484,7 +484,7 @@ if ($vendor eq 'Ubuntu' and ($opt_n or $opt_bn or $opt_qa or $opt_bpo)) {
 # Check the distro name given.
 if (defined $opt_D) {
     if ($vendor eq 'Debian') {
-	unless ($opt_D =~ /^(unstable|((old)?stable|testing)(-security)?|experimental|UNRELEASED|((oldstable|testing)-)?proposed-updates)$/) {
+	unless ($opt_D =~ /^(experimental|unstable|UNRELEASED|((old)?stable|testing)(-proposed-updates|-security)?|proposed-updates)$/) {
 	    my $deb_info = get_debian_distro_info();
 	    my $stable_backports = "";
 	    if ($deb_info == 0) {
@@ -493,8 +493,11 @@ if (defined $opt_D) {
 		$stable_backports = $deb_info->stable() . "-backports";
 	    }
 	    if ($deb_info == 0 || not $opt_D eq $stable_backports) {
-		$stable_backports = " " . $stable_backports . "," if not $stable_backports eq "";
-		warn "$progname warning: Recognised distributions are: unstable, testing, stable,\noldstable, experimental, UNRELEASED,$stable_backports\n{oldstable-,testing-,}proposed-updates and {testing,stable,oldstable}-security.\nUsing your request anyway.\n";
+		$stable_backports = ", " . $stable_backports if not $stable_backports eq "";
+		warn "$progname warning: Recognised distributions are: unstable, testing, stable,\n"
+		     . "oldstable, experimental, {testing-,stable-,oldstable-,}proposed-updates,\n"
+		     . "{testing,stable,oldstable}-security$stable_backports and UNRELEASED.\n"
+		     . "Using your request anyway.\n";
 		$warnings++ if not $opt_force_dist;
 	    }
 	}
