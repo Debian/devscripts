dc995856dd52fc5c4025351b9b9f8b994a4053f8
Philip Hands <phil@hands.com>
2013-02-20 13:25:42 +0000

    ignore "copyright string"

--- scripts/licensecheck.pl
+++ scripts/licensecheck.pl
@@ -330,6 +330,7 @@ sub parse_copyright {
     my $copyright_disindicator_regex = '
 	\b(?:info(?:rmation)?	# Discussing copyright information
 	|(notice|statement)s?	# Discussing the notice
+	|string
 	|and|or                 # Part of a sentence
 	|(holder|owner)s?       # Part of a sentence
 	|ownership              # Part of a sentence
