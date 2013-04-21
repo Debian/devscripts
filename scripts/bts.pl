1e8b1a0a2ea59ca2a678276ae6b6989577aee7c1
James McCoy <jamessan@debian.org>
2013-04-21 10:19:34 -0400

    bts: Accept the "jessie" and "jessie-ignore" tags.
    Closes: #705817
    Signed-off-by: James McCoy <jamessan@debian.org>

--- scripts/bts.pl
+++ scripts/bts.pl
@@ -155,7 +155,7 @@ our (@gTags, @valid_tags, %valid_tags);
            "confirmed", "ipv6", "lfs", "fixed-in-experimental",
            "fixed-upstream", "l10n", "etch", "etch-ignore",
            "lenny", "lenny-ignore", "squeeze", "squeeze-ignore",
-           "wheezy", "wheezy-ignore",
+           "wheezy", "wheezy-ignore", "jessie", "jessie-ignore",
          );
 
 *valid_tags = \@gTags;
