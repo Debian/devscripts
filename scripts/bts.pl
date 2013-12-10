7774ed88a2a9bd9080722e41cc9cd4494b32b352
David Prévot <taffit@debian.org>
2013-12-10 18:24:18 -0400

    tfix: bts.1
    Git-Dch: Ignore

--- scripts/bts.pl
+++ scripts/bts.pl
@@ -1548,7 +1548,7 @@ Adding/removing the B<gift> tag will add/remove the B<gift> usertag,
 belonging to the "debian-qa@lists.debian.org" user.
 
 Adding/removing the B<security> tag will add "team\@security.debian.org"
-to the Cc list of the contol email.
+to the Cc list of the control email.
 
 =cut
 
