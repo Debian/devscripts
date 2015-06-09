Debian/devscripts

commit f45a0b4b71424b50adffe4f8c47cca64ac5fff53

Store cached files in $XDG_CACHE_HOME
Change bts/rc-alert/wnpp-alert to store cached files under
$XDG_CACHE_HOME/devscripts.  Include logic to migrate the existing cache
to the new location on first use so things like “wnpp-alert -d” continue
to work properly during the migration.

Signed-off-by: James McCoy <jamessan@debian.org>

scripts/bts.pl
@@ -46,7 +46,7 @@ use 5.006_000;
 use strict;
 use File::Basename;
 use File::Copy;
-use File::Path;
+use File::Path qw(mkpath make_path rmtree);
 use File::Spec;
 use File::Temp qw/tempfile/;
 use Net::SMTP;
@@ -168,9 +168,11 @@ my @valid_severities=qw(wishlist minor normal important
 
 my $browser;  # Will set if necessary
 
-my $cachedir=$ENV{'HOME'}."/.devscripts_cache/bts/";
-my $timestampdb=$cachedir."bts_timestamps.db";
-my $prunestamp=$cachedir."bts_prune.timestamp";
+my $cachedir = $ENV{XDG_CACHE_HOME} || File::Spec->catdir($ENV{HOME}, '.cache');
+$cachedir = File::Spec->catdir($cachedir, 'devscripts', 'bts');
+
+my $timestampdb = File::Spec->catfile($cachedir, 'bts_timestamps.db');
+my $prunestamp = File::Spec->catfile($cachedir, 'bts_prune.timestamp');
 
 my %timestamp;
 END {
@@ -3684,6 +3686,15 @@ sub browse {
 # this at most once per day for efficiency.
 
 sub prunecache {
+    # TODO: Remove handling of $oldcache post-Stretch
+    my $oldcache = File::Spec->catdir($ENV{HOME}, '.devscripts_cache', 'bts');
+    if (-d $oldcache && ! -d $cachedir) {
+	my $err;
+	make_path(dirname($cachedir), { error => \$err });
+	if (!@$err) {
+	    system('mv', $oldcache, $cachedir);
+	}
+    }
     return unless -d $cachedir;
     return if -f $prunestamp and -M _ < 1;
 

scripts/rc-alert.1
@@ -10,14 +10,14 @@ rc-alert \- check for installed packages with release-critical bugs
 Debian BTS webpages, and then outputs a list of packages installed on
 the system, or given on the command-line, which are in that list.
 .P
-If the directory \fI~/.devscripts_cache\fP exists or the
+If the directory \fI$XDG_CACHE_HOME/devscripts/rc-alert\fP exists or the
 \fB\-\-cache\fP option is given, then the (sizable) downloaded list
 will be cached, and will only be downloaded again on a second
 invocation if it has changed.
 .SH OPTIONS
 .TP
 .BR \-\-cache
-Force the creation of the \fI~/.devscripts_cache\fP cache directory.
+Force the creation of the \fI$XDG_CACHE_HOME/devscripts/rc-alert\fP cache directory.
 .TP
 .BR \-\-help ", " \-h
 Show a summary of options.

scripts/rc-alert.pl
@@ -23,6 +23,9 @@ use strict;
 use warnings;
 use Devscripts::Packages;
 use File::Basename;
+use File::Copy qw(move);
+use File::Path qw(make_path);
+use File::Spec;
 use Getopt::Long qw(:config gnu_getopt);
 
 sub remove_duplicate_values($);
@@ -31,9 +34,11 @@ sub human_flags($);
 sub unhtmlsanit($);
 sub dt_parse_request($);
 
-my $cachedir = $ENV{'HOME'}."/.devscripts_cache/";
+my $cachedir = $ENV{XDG_CACHE_HOME} || File::Spec->catdir($ENV{HOME}, '.cache');
+$cachedir = File::Spec->catdir($cachedir, 'devscripts', 'rc-alert');
+
 my $url = "http://bugs.debian.org/release-critical/other/all.html";
-my $cachefile = $cachedir . basename($url);
+my $cachefile = File::Spec->catfile($cachedir, basename($url));
 my $forcecache = 0;
 my $usecache = 0;
 
@@ -171,10 +176,15 @@ if (system("command -v wget >/dev/null 2>&1") == 0) {
     die "$progname: this program requires either the wget or curl package to be installed\n";
 }
 
-
-if (! -d $cachedir and $forcecache) {
-    mkdir $cachedir
-	or die "$progname: can't make cache directory $cachedir: $!\n";
+# TODO: Remove oldcache handling post-Stretch
+my $oldcache = File::Spec->catfile($ENV{HOME}, '.devscripts_cache', 'all.html');
+if (! -d $cachedir) {
+    if (-f $oldcache || $forcecache) {
+	make_path($cachedir);
+	if (-f $oldcache) {
+	    move($oldcache, $cachefile);
+	}
+    }
 }
 
 if (-d $cachedir) {

scripts/wnpp-alert.1
@@ -17,7 +17,7 @@ package based.
 .SH OPTIONS
 .TP
 .BR \-\-diff ", " \-d
-If the \fI~/.devscripts_cache\fP directory exists, compare the output of
+If the \fI$XDG_CACHE_HOME/devscripts\fP directory exists, compare the output of
 \fBwnpp-alert\fR to the previous output (cached in the file
 \fIwnpp-diff\fR) and output the differences.
 .TP

scripts/wnpp-alert.sh
@@ -16,7 +16,11 @@
 set -e
 
 PROGNAME="${0##*/}"
-CACHEDIR=~/.devscripts_cache
+# TODO: Remove use of OLDCACHEDDIR post-Stretch
+OLDCACHEDIR=~/.devscripts_cache
+OLDCACHEDDIFF="${OLDCACHEDIR}/wnpp-diff"
+CACHEDIR=${XDG_CACHE_HOME:-$HOME/.cache}
+CACHEDIR=${CACHEDIR%/}/devscripts
 CACHEDDIFF="${CACHEDIR}/wnpp-diff"
 CURLORWGET=""
 GETCOMMAND=""
@@ -37,6 +41,9 @@ Modifications: Julian Gilbey <jdg@debian.org>"
 }
 
 wnppdiff () {
+    if [ -f "$OLDCACHEDDIFF" ]; then
+        mv "$OLDCACHEDDIFF" "$CACHEDDIFF"
+    fi
     if [ ! -f "$CACHEDDIFF" ]; then
         # First use
         comm -12 $WNPP_PACKAGES $INSTALLED | sed -e 's/+/\\+/g' | \
@@ -120,7 +127,10 @@ else
 fi
 
 if [ -f "$WNPP_DIFF" ]; then
-    if [ -d "$CACHEDIR" ]; then
+    # This may fail when run from a cronjob (c.f., #309802), so just ignore it
+    # and carry on.
+    mkdir -p "$CACHEDIR" >/dev/null 2>&1 || true
+    if [ -d "$CACHEDIR" ] || [ -d "$OLDCACHEDIR" ]; then
         wnppdiff
         exit 0
     else

commit 6274145dfdfe52686ec51a3adfea9e041c37fa9d

bts: Use File::Spec in cachefile/mboxfile/cachebugdir
f45a0b4b71424b50adffe4f8c47cca64ac5fff53 caused path separators not to
be included between the cachedir and the trailing element in the
mentioned functions.  Use of File::Spec (catfile or catdir, accordingly)
ensures the correct separator is included.

Signed-off-by: James McCoy <jamessan@debian.org>

scripts/bts.pl
@@ -3384,20 +3384,20 @@ sub cachefile {
     $thing =~ s%/%_%g;
     $thgopts =~ s/;/_3B/g;
     $thgopts =~ s/=/_3D/g;
-    return $cachedir.$thing.$thgopts.($thing =~ /\.html$/ ? "" : ".html");
+    return File::Spec->catfile($cachedir, $thing.$thgopts.($thing =~ /\.html$/ ? "" : ".html"));
 }
 
 # Given a thing, returns the filename for its mbox in the cache.
 sub mboxfile {
     my $thing=shift;
-    return $thing =~ /^\d+$/ ? $cachedir.$thing.".mbox" : undef;
+    return $thing =~ /^\d+$/ ? File::Spec->catfile($cachedir, $thing.".mbox") : undef;
 }
 
 # Given a bug number, returns the dirname for it in the cache.
 sub cachebugdir {
     my $thing=shift;
     if ($thing !~ /^\d+$/) { die "$progname: cachebugdir given faulty argument: $thing\n"; }
-    return $cachedir.$thing;
+    return File::Spec->catdir($cachedir, $thing);
 }
 
 # And the reverse: Given a filename in the cache, returns the corresponding
@@ -3783,7 +3783,7 @@ sub runbrowser {
 sub runmailreader {
     my $file = shift;
     my $quotedfile;
-    die "$progname: could not read mbox file!\n" unless -r $file;
+    die "$progname: could not read mbox file $file!\n" unless -r $file;
 
     if ($file !~ /\'/) { $quotedfile = qq['$file']; }
     elsif ($file !~ /[\"\\\$\'\!]/) { $quotedfile = qq["$file"]; }