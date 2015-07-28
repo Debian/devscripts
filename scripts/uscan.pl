Debian/devscripts

commit 7c250fa9d0b31aad0a3ad53d2aeb33ee140dd6dc

Remove manual parsing of dpkg-buildpackage's output
Use its -S switch or, for perl scripts, the Dpkg::Changelog::Parse
module instead.

Signed-off-by: James McCoy <jamessan@debian.org>

scripts/uscan.pl
@@ -27,6 +27,7 @@ use strict;
 use warnings;
 use Cwd;
 use Cwd 'abs_path';
+use Dpkg::Changelog::Parse qw(changelog_parse);
 use Dpkg::IPC;
 use File::Basename;
 use File::Copy;
@@ -463,14 +464,14 @@ if (defined $opt_watchfile) {
 	}
 
 	# Figure out package info we need
-	my $changelog = `dpkg-parsechangelog`;
-	unless ($? == 0) {
-	    uscan_die "$progname: Problems running dpkg-parsechangelog\n";
+	my $changelog = eval { changelog_parse(); };
+	if ($@) {
+	    uscan_die "$progname: Problems parsing debian/changelog: $@\n";
 	}
 
 	my ($package, $debversion, $uversion);
-	$changelog =~ /^Source: (.*?)$/m and $package=$1;
-	$changelog =~ /^Version: (.*?)$/m and $debversion=$1;
+	$package = $changelog->{Source};
+	$debversion = $changelog->{Version};
 	if (! defined $package || ! defined $debversion) {
 	    uscan_die "$progname: Problems determining package name and/or version from\n  debian/changelog\n";
 	}
@@ -546,15 +547,15 @@ for my $dir (@dirs) {
     # Check for debian/watch file
     if (-r 'debian/watch' and -r 'debian/changelog') {
 	# Figure out package info we need
-	my $changelog = `dpkg-parsechangelog`;
-	unless ($? == 0) {
-	    uscan_warn "$progname warning: Problems running dpkg-parsechangelog in $dir, skipping\n";
+	my $changelog = eval { changelog_parse(); };
+	if ($@) {
+	    uscan_warn "$progname warning: Problems parse debian/changelog in $dir, skipping\n";
 	    next;
 	}
 
 	my ($package, $debversion, $uversion);
-	$changelog =~ /^Source: (.*?)$/m and $package=$1;
-	$changelog =~ /^Version: (.*?)$/m and $debversion=$1;
+	$packages = $changelog->{Source};
+	$debversion = $changelog->{Version};
 	if (! defined $package || ! defined $debversion) {
 	    uscan_warn "$progname warning: Problems determining package name and/or version from\n  $dir/debian/changelog, skipping\n";
 	    next;

Makefile
@@ -10,8 +10,7 @@ all: version make_scripts $(EXAMPLES) translated_manpages
 
 version:
 	rm -f version
-	dpkg-parsechangelog | perl -ne '/^Version: (.*)/ && print $$1' \
-	    > version
+	dpkg-parsechangelog -SVersion > version
 
 conf.default: conf.default.in version
 	rm -f $@ $@.tmp

debian/control
@@ -11,6 +11,7 @@ Standards-Version: 3.9.6
 Build-Depends: bash-completion,
                debhelper (>= 9),
                docbook-xsl,
+               dpkg-dev (>= 1.17.0),
                file,
                libdistro-info-perl,
                libfile-desktopentry-perl,
@@ -39,7 +40,7 @@ Vcs-Git: git://anonscm.debian.org/collab-maint/devscripts.git
 Package: devscripts
 Architecture: any
 Multi-Arch: foreign
-Depends: dpkg-dev (>= 1.16.4),
+Depends: dpkg-dev (>= 1.17.0),
          ${misc:Depends},
          ${perl:Depends},
          ${python3:Depends},

scripts/cvs-debi.sh
@@ -220,20 +220,20 @@ if [ "x$opt_package" = "x" ]; then
     if [ -f debian/changelog ]; then
 	# Ok, changelog exists
 	 setq "package" \
-	    "`dpkg-parsechangelog | sed -n 's/^Source: //p'`" \
+	    "`dpkg-parsechangelog -SSource`" \
 		"source package"
 	setq "version" \
-	    "`dpkg-parsechangelog | sed -n 's/^Version: //p'`" \
+	    "`dpkg-parsechangelog -SVersion`" \
 		"source version"
     elif [ "x$opt_cvsmodule" != "x" ]; then
 	# Hmm. Well, see if we can checkout the changelog file
 	rm -f $TEMPFILE
 	cvs -q co -p $TAGOPT $opt_cvsmodule/debian/changelog > $TEMPFILE
         setq "package" \
-	    "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Source: //p'`" \
+	    "`dpkg-parsechangelog -l$TEMPFILE -SSource`" \
           "source package"
         setq "version" \
-          "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Version: //p'`" \
+          "`dpkg-parsechangelog -l$TEMPFILE -SVersion`" \
           "source version"
         rm -f "$TEMPFILE"
     else
@@ -256,14 +256,14 @@ else
     elif [ -f debian/changelog ]; then
 	# Fine, see what the changelog says
 	setq "version" \
-	    "`dpkg-parsechangelog | sed -n 's/^Version: //p'`" \
+	    "`dpkg-parsechangelog -SVersion`" \
 		"source version"
     elif [ "x$opt_cvsmodule" != "x" ]; then
 	# Hmm. The CVS module name is known, so lets us try exporting changelog
 	rm -f $TEMPFILE
 	cvs -q co -p $TAGOPT $opt_cvsmodule/debian/changelog > $TEMPFILE
         setq "version" \
-          "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Version: //p'`" \
+          "`dpkg-parsechangelog -l$TEMPFILE -SVersion`" \
           "source version"
         rm -f "$TEMPFILE"
     else
@@ -272,7 +272,7 @@ else
 	cvsmodule="${prefix}$package"
 	cvs -q co -p $TAGOPT $cvsmodule/debian/changelog > $TEMPFILE
         setq "version" \
-          "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Version: //p'`" \
+          "`dpkg-parsechangelog -l$TEMPFILE -SVersion`" \
           "source version"
         rm -f "$TEMPFILE"
     fi

scripts/cvs-debrelease.sh
@@ -227,20 +227,20 @@ if [ "x$opt_package" = "x" ]; then
     if [ -f debian/changelog ]; then
 	# Ok, changelog exists
 	 setq "package" \
-	    "`dpkg-parsechangelog | sed -n 's/^Source: //p'`" \
+	    "`dpkg-parsechangelog -SSource`" \
 		"source package"
 	setq "version" \
-	    "`dpkg-parsechangelog | sed -n 's/^Version: //p'`" \
+	    "`dpkg-parsechangelog -SVersion`" \
 		"source version"
     elif [ "x$opt_cvsmodule" != "x" ]; then
 	# Hmm. Well, see if we can checkout the changelog file
 	rm -f $TEMPFILE
 	cvs -q co -p $TAGOPT $opt_cvsmodule/debian/changelog > $TEMPFILE
         setq "package" \
-	    "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Source: //p'`" \
+	    "`dpkg-parsechangelog -l$TEMPFILE -SSource`" \
           "source package"
         setq "version" \
-          "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Version: //p'`" \
+          "`dpkg-parsechangelog -l$TEMPFILE -SVersion`" \
           "source version"
         rm -f "$TEMPFILE"
     else
@@ -263,14 +263,14 @@ else
     elif [ -f debian/changelog ]; then
 	# Fine, see what the changelog says
 	setq "version" \
-	    "`dpkg-parsechangelog | sed -n 's/^Version: //p'`" \
+	    "`dpkg-parsechangelog -SVersion`" \
 		"source version"
     elif [ "x$opt_cvsmodule" != "x" ]; then
 	# Hmm. The CVS module name is known, so lets us try exporting changelog
 	rm -f $TEMPFILE
 	cvs -q co -p $TAGOPT $opt_cvsmodule/debian/changelog > $TEMPFILE
         setq "version" \
-          "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Version: //p'`" \
+          "`dpkg-parsechangelog -l$TEMPFILE -SVersion`" \
           "source version"
         rm -f "$TEMPFILE"
     else
@@ -279,7 +279,7 @@ else
 	cvsmodule="${prefix}$package"
 	cvs -q co -p $TAGOPT $cvsmodule/debian/changelog > $TEMPFILE
         setq "version" \
-          "`dpkg-parsechangelog -l$TEMPFILE | sed -n 's/^Version: //p'`" \
+          "`dpkg-parsechangelog -l$TEMPFILE -SVersion`" \
           "source version"
         rm -f "$TEMPFILE"
     fi

scripts/debchange.bash_completion
@@ -51,7 +51,7 @@ _debchange()
 	    ;;
 #FIXME: we need "querybts --list" option with no verbose output
 #	--closes)
-#	     package=`dpkg-parsechangelog |grep ^Source|cut -d':' -f2|sed -e s/\ //`
+#	     package=`dpkg-parsechangelog -SSource`
 #	     bugnumber=`querybts --list -b $package|grep ^#|cut -d' ' -f1`
 #	     COMPREPLY=( $( compgen -W "$bugnumber" ) )
 #	    ;;

scripts/debchange.pl
@@ -40,7 +40,7 @@ use File::Copy;
 use File::Basename;
 use Cwd;
 use Dpkg::Vendor qw(get_current_vendor);
-use Dpkg::Changelog::Parse;
+use Dpkg::Changelog::Parse qw(changelog_parse);
 use Dpkg::Control;
 use Devscripts::Compression;
 use Devscripts::Debbugs;
@@ -613,7 +613,7 @@ my $CL_URGENCY = '';
 
 if (! $opt_create || ($opt_create && $opt_news)) {
     my $file = $opt_create ? 'debian/changelog' : $changelog_path;
-    $changelog = Dpkg::Changelog::Parse::changelog_parse(file => $file);
+    $changelog = changelog_parse(file => $file);
 
     # Now we've read the changelog, set some variables and then
     # let's check the directory name is sensible
@@ -644,7 +644,7 @@ if (! $opt_create || ($opt_create && $opt_news)) {
     if ($opt_news) {
 	my $found_version = 0;
 	my $found_urgency = 0;
-	my $clog = Dpkg::Changelog::Parse::changelog_parse(file => $real_changelog_path);
+	my $clog = changelog_parse(file => $real_changelog_path);
 	$VERSION = $clog->{Version};
 	$VERSION =~ s/~$//;
 
@@ -1555,15 +1555,10 @@ if ((basename(cwd()) =~ m%^\Q$PACKAGE\E-\Q$UVERSION\E$%) &&
     !$opt_p && !$opt_create) {
     # Find the current version number etc.
     my ($new_version, $new_sversion, $new_uversion);
-    open PARSED, "dpkg-parsechangelog |"
-	or fatal "Cannot execute dpkg-parsechangelog: $!";
-    while (<PARSED>) {
-	if (/^Version:\s(.+?)\s*$/) { $new_version=$1; }
+    my $changelog = changelog_parse();
+    if (exists $changelog->{Version}) {
+	$new_version = $changelog->{Version};
     }
-
-    close PARSED
-	or fatal "Problem executing dpkg-parsechangelog: $!";
-    if ($?) { fatal "dpkg-parsechangelog failed!" }
 
     fatal "No version number in debian/changelog!"
 	unless defined $new_version;

scripts/debclean.sh
@@ -172,7 +172,7 @@ for i in `find . -type d -name "debian"`; do
 	echo "Directory $DIR: contains no debian/changelog, skipping" >&2
 	exit
     fi
-    package="`dpkg-parsechangelog | sed -n 's/^Source: //p'`"
+    package="`dpkg-parsechangelog -SSource`"
     if [ -z "$package" ]; then
 	echo "Directory $DIR: unable to determine package name, skipping" >&2
 	exit

commit c9b52e2576e3af1a6c1f5f1177b9e3ab014a4292

uscan: Fix spelling of $packages
Regression introduced in 7c250fa9d0b31aad0a3ad53d2aeb33ee140dd6dc

Signed-off-by: James McCoy <jamessan@debian.org>

scripts/uscan.pl
@@ -554,7 +554,7 @@ for my $dir (@dirs) {
 	}
 
 	my ($package, $debversion, $uversion);
-	$packages = $changelog->{Source};
+	$package = $changelog->{Source};
 	$debversion = $changelog->{Version};
 	if (! defined $package || ! defined $debversion) {
 	    uscan_warn "$progname warning: Problems determining package name and/or version from\n  $dir/debian/changelog, skipping\n";