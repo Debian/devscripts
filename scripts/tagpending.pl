1a997e21b7f05e54a6344a6ece396939d2acd64e
James McCoy <jamessan@debian.org>
2015-08-14 21:55:47 -0400

    tagpending: Only query the BTS if there are closed bugs
    Signed-off-by: James McCoy <jamessan@debian.org>

--- scripts/tagpending.pl
+++ scripts/tagpending.pl
@@ -169,25 +169,27 @@ $changes .= "   *";
 
 my $pending;
 my $open;
+my %bugs = map { $_ => 1 } @closes;
 
-if ($opt_online) {
-    if (!Devscripts::Debbugs::have_soap()) {
-	die "$progname: The libsoap-lite-perl package is required for online operation; aborting.\n";
+if (%bugs) {
+    if ($opt_online) {
+	if (!Devscripts::Debbugs::have_soap()) {
+	    die "$progname: The libsoap-lite-perl package is required for online operation; aborting.\n";
+	}
+
+	eval {
+	    $pending = Devscripts::Debbugs::select( "src:$source", "status:open", "status:forwarded", "tag:pending" );
+	    $open = Devscripts::Debbugs::select( "src:$source", "status:open", "status:forwarded" );
+	};
+
+	if ($@) {
+	    die "$@\nUse --force to tag all bugs anyway.\n";
+	}
     }
 
-    eval {
-	$pending = Devscripts::Debbugs::select( "src:$source", "status:open", "status:forwarded", "tag:pending" );
-	$open = Devscripts::Debbugs::select( "src:$source", "status:open", "status:forwarded" );
-    };
-
-    if ($@) {
-	die "$@\nUse --force to tag all bugs anyway.\n";
+    if ($pending) {
+	%bugs = ( %bugs, map { $_ => 1 } @{$pending} );
     }
-}
-
-my %bugs = map { $_ => 1} @closes;
-if ($pending) {
-    %bugs = ( %bugs, map { $_ => 1} @{$pending} );
 }
 
 my $bug;
