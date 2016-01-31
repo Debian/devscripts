e08bf90588462914704841bebe37126360ffd254
James McCoy <jamessan@debian.org>
2016-01-31 15:17:55 -0500

    mk-build-deps: Add Debug::pkgProblemResolver to documentation
    Signed-off-by: James McCoy <jamessan@debian.org>

--- scripts/mk-build-deps.pl
+++ scripts/mk-build-deps.pl
@@ -52,7 +52,7 @@ Install the generated packages and its build-dependencies.
 =item B<-t>, B<--tool>
 
 When installing the generated package use the specified tool.
-(default: B<apt-get --no-install-recommends>)
+(default: B<apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends>)
 
 =item B<-r>, B<--remove>
 
