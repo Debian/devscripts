Debian/devscripts

commit 2ef682e5c37f45dcee19198f90afb292bf2c34a2

sadt: add support for Test-Command: in debian/tests/control
This involves extracting the logic for running a test into a TestCommand
class, which can handle any command, and a Test subclass, which handles
the special case of a script under debian/tests

Closes: #781963

scripts/sadt
@@ -153,6 +153,101 @@ class VerboseProgress(Progress):
         self._write('\n')
 
 
+class TestCommand(object):
+
+    def __init__(self, group, command):
+        self.group = group
+        self.command = command
+
+    def __str__(self):
+        return self.command
+
+    def get_command(self):
+        return ['sh', '-c', self.command]
+
+    def prepare(self, progress, rw_build_tree):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def run(self, progress, allow_stderr=False):  # pylint: disable=too-many-locals
+        command = self.get_command()
+        tmpdir1 = tempfile.mkdtemp(prefix='sadt.')
+        tmpdir2 = tempfile.mkdtemp(prefix='sadt.')
+        environ = dict(os.environ)
+        environ['AUTOPKGTEST_TMP'] = tmpdir1
+        environ['ADTTMP'] = tmpdir2  # only for compatibility with old DEP-8 spec.
+        child = ipc.Popen(command, stdout=ipc.PIPE, stderr=ipc.PIPE, env=environ)
+        output = []
+        stderr = False
+        for tag, line in annotate_output(child):
+            if tag == 'E':
+                stderr = True
+            this_line = '{tag}: {line}'.format(
+                tag=tag,
+                line=line.decode(sys.stdout.encoding, 'replace'),
+            )
+            progress.output(this_line)
+            output.append(this_line)
+        for fp in child.stdout, child.stderr:
+            fp.close()
+        rc = child.wait()
+        shutil.rmtree(tmpdir1)
+        shutil.rmtree(tmpdir2)
+        if rc == 0:
+            if stderr and not allow_stderr:
+                rc = -1
+                fail_reason = 'stderr non-empty'
+                progress.fail(self, fail_reason)
+            else:
+                progress.ok(self)
+        else:
+            fail_reason = 'exit code: {rc}'.format(rc=rc)
+            progress.fail(self, fail_reason)
+        if rc != 0:
+            raise Fail(self, fail_reason, ''.join(output))
+
+
+class Test(TestCommand):
+
+    def __init__(self, group, testname):
+        self.testname = testname
+        self.original_mode = None
+        self.cwd = None
+        super(Test, self).__init__(group, testname)
+
+    @property
+    def path(self):
+        return os.path.join(self.group.tests_directory, self.testname)
+
+    def __str__(self):
+        return self.testname
+
+    def get_command(self):
+        return [self.path]
+
+    def prepare(self, progress, rw_build_tree):
+        if rw_build_tree:
+            self.cwd = os.getcwd()
+            os.chdir(rw_build_tree)
+            chmod_x(self.path)
+        else:
+            if not os.access(self.path, os.X_OK):
+                try:
+                    self.original_mode = chmod_x(self.path)
+                except OSError as exc:
+                    progress.skip(self.testname, '{path} could not be made executable: {exc}'
+                                  .format(path=self.path, exc=exc))
+                    raise Skip
+
+    def cleanup(self):
+        if self.original_mode is not None:
+            os.chmod(self.path, self.original_mode)
+        if self.cwd is not None:
+            os.chdir(self.cwd)
+
+
 class TestGroup(object):
 
     def __init__(self):
@@ -257,70 +352,19 @@ class TestGroup(object):
         except Skip as exc:
             progress.skip(test, str(exc))
             raise
-        path = os.path.join(self.tests_directory, test)
-        original_mode = None
-        if rw_build_tree:
-            cwd = os.getcwd()
-            os.chdir(rw_build_tree)
-            chmod_x(path)
-        else:
-            cwd = None
-            if not os.access(path, os.X_OK):
-                try:
-                    original_mode = chmod_x(path)
-                except OSError as exc:
-                    progress.skip(test, '{path} could not be made executable: {exc}'
-                                  .format(path=path, exc=exc))
-                    raise Skip
+        test.prepare(progress, rw_build_tree)
         try:
             progress.start(test)
-            self._run(test, progress, allow_stderr=options.allow_stderr)
+            test.run(progress, allow_stderr=options.allow_stderr)
         finally:
-            if original_mode is not None:
-                os.chmod(path, original_mode)
-            if cwd is not None:
-                os.chdir(cwd)
-
-    def _run(self, test, progress, allow_stderr=False):  # pylint: disable=too-many-locals
-        path = os.path.join(self.tests_directory, test)
-        tmpdir1 = tempfile.mkdtemp(prefix='sadt.')
-        tmpdir2 = tempfile.mkdtemp(prefix='sadt.')
-        environ = dict(os.environ)
-        environ['AUTOPKGTEST_TMP'] = tmpdir1
-        environ['ADTTMP'] = tmpdir2  # only for compatibility with old DEP-8 spec.
-        child = ipc.Popen([path], stdout=ipc.PIPE, stderr=ipc.PIPE, env=environ)
-        output = []
-        stderr = False
-        for tag, line in annotate_output(child):
-            if tag == 'E':
-                stderr = True
-            this_line = '{tag}: {line}'.format(
-                tag=tag,
-                line=line.decode(sys.stdout.encoding, 'replace'),
-            )
-            progress.output(this_line)
-            output.append(this_line)
-        for fp in child.stdout, child.stderr:
-            fp.close()
-        rc = child.wait()
-        shutil.rmtree(tmpdir1)
-        shutil.rmtree(tmpdir2)
-        if rc == 0:
-            if stderr and not allow_stderr:
-                rc = -1
-                fail_reason = 'stderr non-empty'
-                progress.fail(test, fail_reason)
-            else:
-                progress.ok(test)
-        else:
-            fail_reason = 'exit code: {rc}'.format(rc=rc)
-            progress.fail(test, fail_reason)
-        if rc != 0:
-            raise Fail(test, fail_reason, ''.join(output))
+            test.cleanup()
 
     def add_tests(self, tests):
-        tests = re.split(r'\s*,?\s+', tests)
+        tests = [Test(self, t) for t in re.split(r'\s*,?\s+', tests)]
         self.tests = frozenset(tests)
+
+    def add_test_command(self, test_command):
+        self.tests = frozenset([TestCommand(self, test_command)])
 
     def add_restrictions(self, restrictions):
         restrictions = re.split(r'\s*,?\s+', restrictions)

test/test_sadt
@@ -60,4 +60,15 @@ test_comma_separated_restrictions() {
     assertFalse "skipped=1 found in output" "grep -q skipped=1 $log"
 }
 
+test_tests_directory() {
+    assertPasses run_sadt tests-directory
+    assertFalse "skipped=1 found in output" "grep -q skipped=1 $log"
+}
+
+test_test_command() {
+    assertPasses run_sadt test-command --verbose
+    assertTrue "recognizes Test-Command" "grep 'O: Test-Command is supported' $log"
+    assertTrue "recognizes Test-Command" "grep tests=1 $log"
+}
+
 . shunit2

test/sadt/test-command/debian/tests/control
@@ -1,0 +1,1 @@
+Test-Command: echo 'Test-Command is supported'

test/sadt/tests-directory/debian/tests/control
@@ -1,0 +1,2 @@
+Tests: test
+Tests-Directory: foo

test/sadt/tests-directory/foo/test
@@ -1,0 +1,1 @@
+#!/bin/true

commit 46d0a922947826f0f1b05eab390c945da07db894

sadt: fix filtering test by name
This bug was introduced by commit 2ef682e5c37f45dcee19198f90afb292bf2c34a2.

Tests are now proper objects instead of just strings, we can no longer
compare them directly to the names of the tests passed in the command
line.

scripts/sadt
@@ -193,6 +193,10 @@ class TestCommand(object):
     def __str__(self):
         return self.command
 
+    @property
+    def name(self):
+        return str(self)
+
     def get_command(self):
         return ['sh', '-c', self.command]
 
@@ -483,8 +487,8 @@ def main():
     rw_build_tree = None
     try:
         for group in test_groups:
-            for name in group:
-                if options.tests and name not in options.tests:
+            for test in group:
+                if options.tests and test.name not in options.tests:
                     continue
                 try:
                     if rw_build_tree is None:
@@ -497,7 +501,7 @@ def main():
                                 rw_build_tree = copy_build_tree()
                                 assert rw_build_tree is not None
                     group.run(
-                        name,
+                        test,
                         progress=progress,
                         ignored_restrictions=options.ignore_restrictions,
                         rw_build_tree=rw_build_tree,
@@ -506,7 +510,7 @@ def main():
                 except Skip:
                     n_skip += 1
                 except Fail as exc:
-                    failures += [(name, exc)]
+                    failures += [(test, exc)]
                 else:
                     n_ok += 1
     finally:

test/test_sadt
@@ -71,4 +71,10 @@ test_test_command() {
     assertTrue "recognizes Test-Command" "grep tests=1 $log"
 }
 
+test_run_specific_tests() {
+    assertPasses run_sadt comma-separated-tests --verbose test1
+    assertTrue "runs test1" "grep '^test1:' $log"
+    assertFalse "does not run test2" "grep '^test2:' $log"
+}
+
 . shunit2