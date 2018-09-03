Debian/devscripts

commit fba3608b8c5f8b5b16b9b261b41d468d28ba3a8f

rmadison: Simplify check for known SSL error codes
Signed-off-by: James McCoy <jamessan@debian.org>

scripts/rmadison.pl
@@ -221,14 +221,13 @@ foreach my $url (@url) {
         push @ssl_errors, 5;
     }
     system @cmd, $url . (($url =~ m/\?/)?'&':'?')."package=" . join("+", map { uri_escape($_) } @ARGV) . "&text=on&" . join ("&", @args);
-    foreach (@ssl_errors)
-    {
-        if ($? >> 8 == $_)
-        {
-           die "Problem with SSL CACERT check:\n Have you installed the ca-certificates package?\n";
+    my $rc = $? >> 8;
+    if ($rc != 0) {
+        if (grep $rc, @ssl_errors) {
+            die "Problem with SSL CACERT check:\n Have you installed the ca-certificates package?\n";
         }
+        $status = 1;
     }
-    $status = 1 if ($? >> 8 != 0);
 }
 
 exit $status;

commit 56e6defb0deaabba0ac601ff44a4ff8b6c6c2e52

rmadison: Fix error handling to avoid having all curl/wget errors to be treated as SSL errors.
Closes: #907683
Fixes: fba3608b8c5f8b5b16b9b261b41d468d28ba3a8f
Signed-off-by: Jakub Wilk <jwilk@jwilk.net>
Signed-off-by: Mattia Rizzolo <mattia@debian.org>

scripts/rmadison.pl
@@ -223,7 +223,7 @@ foreach my $url (@url) {
     system @cmd, $url . (($url =~ m/\?/)?'&':'?')."package=" . join("+", map { uri_escape($_) } @ARGV) . "&text=on&" . join ("&", @args);
     my $rc = $? >> 8;
     if ($rc != 0) {
-        if (grep $rc, @ssl_errors) {
+        if (grep { $_ == $rc } @ssl_errors) {
             die "Problem with SSL CACERT check:\n Have you installed the ca-certificates package?\n";
         }
         $status = 1;