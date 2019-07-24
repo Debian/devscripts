Debian/devscripts

commit 25d09affb98dfcfd17dfe3ee4646fe0fd0f9ed73

debi: replace dpkg + apt-get by "apt-get install" on .change file
Closes: #810294

scripts/debi.pl
@@ -34,6 +34,7 @@ use Dpkg::Control;
 use Dpkg::Changelog::Parse qw(changelog_parse);
 use IPC::Run qw(run);
 use IO::Handle;
+use Dpkg::IPC;
 
 my $progname = basename($0, '.pl');    # the '.pl' is for when we're debugging
 my $modified_conf_msg;
@@ -128,7 +129,7 @@ my $debsdir;
 my $debsdir_warning;
 my $check_dirname_level = 1;
 my $check_dirname_regex = 'PACKAGE(-.+)?';
-my $install_tool        = 'apt-get';
+my $install_tool        = (-t STDOUT ? 'apt' : 'apt-get');
 
 # Next, read configuration files and then command line
 # The next stuff is boilerplate
@@ -411,13 +412,19 @@ if (!@debs) {
 if ($progname eq 'debi') {
     my @upgrade = $opt_upgrade ? ('-O') : ();
     if ($opt_with_depends) {
-        system('debpkg', @upgrade, '--unpack', @debs) == 0
-          or die "$progname: debpkg --unpack failed \n";
-        system($install_tool, '-f', 'install') == 0
-          or die "$progname: " . $install_tool . ' -f install failed\n';
+        if ($install_tool =~ /^apt(?:-get)?$/) {
+            spawn(exec => [$install_tool, 'install', "./$changes"]);
+        } else {
+            spawn(exec => ['debpkg', @upgrade, '--unpack', @debs]);
+            spawn(exec => [$install_tool, '-f', 'install']);
+        }
     } else {
-        system('debpkg', @upgrade, '-i', @debs) == 0
-          or die "$progname: debpkg -i failed\n";
+        if ($install_tool =~ /^apt(?:-get)?$/) {
+            spawn(exec =>
+                  [$install_tool, 'install', '--only-upgrade', "./$changes"]);
+        } else {
+            spawn(exec => ['debpkg', @upgrade, '-i', @debs]);
+        }
     }
 } else {
     # $progname eq 'debc'

commit 30c3bc4c093796eec61a22738a3d530cfe67888e

debi: Invoke apt or apt-get with --reinstall
Otherwise, if you are doing repeated test-builds of the same package
with the intended next version number, apt will optimize away the
reinstallation into nothing and it will not actually be reinstalled.

Signed-off-by: Simon McVittie <smcv@debian.org>
Fixes: 25d09aff "debi: replace dpkg + apt-get by "apt-get install" on .change file"

scripts/debi.pl
@@ -414,7 +414,8 @@ if ($progname eq 'debi') {
     if ($opt_with_depends) {
         if ($install_tool =~ /^apt(?:-get)?$/) {
             spawn(
-                exec       => [$install_tool, 'install', "./$changes"],
+                exec =>
+                  [$install_tool, 'install', '--reinstall', "./$changes"],
                 wait_child => 1
             );
         } else {
@@ -427,8 +428,11 @@ if ($progname eq 'debi') {
     } else {
         if ($install_tool =~ /^apt(?:-get)?$/) {
             spawn(
-                exec =>
-                  [$install_tool, 'install', '--only-upgrade', "./$changes"],
+                exec => [
+                    $install_tool,    'install',
+                    '--only-upgrade', '--reinstall',
+                    "./$changes"
+                ],
                 wait_child => 1
             );
         } else {