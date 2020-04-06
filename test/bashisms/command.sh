de03e363ff8a07c5732a3275d2685e7fe116864d
Mattia Rizzolo <mattia@debian.org>
2020-04-06 15:35:02 +0200

    checkbashisms: Improve `command` recognition.
    + Improve check for `command` to properly detect options other than -p
      also when -p is not the first option.
    + After Policy v4.1.5, the POSIX standard for shell script is
      POSIX.1-2017.  Recognize `command -v` and `command -V` as valid.
    
    Closes: #835498
    Thanks: Eero Vuojolahti <eero@vuojolahti.com> for the initial patch
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- test/bashisms/command.sh
+++ test/bashisms/command.sh
@@ -2,5 +2,15 @@
 
 command test
 command -p test
-command -v test # BASHISM
-command -V test # BASHISM
+command -v test
+command -V test
+command -p test
+command -p -v test
+command -pv test
+command -p -v -a test # BASHISM
+command -p -a -v test # BASHISM
+command -pa test # BASHISM
+command -ap test # BASHISM
+command -p -a test # BASHISM
+command -pV -a test # BASHISM
+command -p test
