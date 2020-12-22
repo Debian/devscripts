a903b80bd4b8cec34fb27cc60d1a9196c60890bf
mimi89999 <michel@lebihan.pl>
2020-12-22 11:13:19 +0100

    debchange: Warn when DEBEMAIL and EMAIL are not set

--- scripts/debchange.pl
+++ scripts/debchange.pl
@@ -842,12 +842,18 @@ if (!$opt_m and !$opt_M) {
     } elsif (exists $env{'EMAIL'}) {
         $EMAIL = $env{'EMAIL'};
     } else {
+        warn "$progname warning: neither DEBEMAIL nor EMAIL environment variable is set\n";
+        $warnings++;
         my $addr;
         if (open MAILNAME, '/etc/mailname') {
+            warn "$progname warning: building email address from username and mailname\n";
+            $warnings++;
             chomp($addr = <MAILNAME>);
             close MAILNAME;
         }
         if (!$addr) {
+            warn "$progname warning: building email address from username and FQDN\n";
+            $warnings++;
             chomp($addr = `hostname --fqdn 2>/dev/null`);
             $addr = undef if $?;
         }
