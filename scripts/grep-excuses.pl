367cb354e8b02e6991d256181396d3492681f7f4
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-09-08 10:57:19 +0100

    grep-excuses --autopkgtests: Do not report RUNNING
    These are noise.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- scripts/grep-excuses.pl
+++ scripts/grep-excuses.pl
@@ -391,7 +391,7 @@ if ($do_autopkgtests) {
             my $arches = $autopkgtests->{$k};
             foreach my $arch (sort keys %$arches) {
                 my $info = $arches->{$arch};
-                next if $info->[0] eq 'PASS';
+                next if $info->[0] eq 'PASS' or $info->[0] eq 'RUNNING';
                 printf "\nautopkgtest regression\n";
                 printf "    in %s (%s) on %s\n", $testpkg, $testvsn, $arch;
                 printf "    due to %s\n", migration_headline($source);
