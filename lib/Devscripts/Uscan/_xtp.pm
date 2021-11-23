8246ebbe2b4aac700b8a5e4358e4152e71979737
Mattia Rizzolo <mattia@debian.org>
2021-11-23 13:03:34 +0100

    Revert "uscan: Assign $newfile_base a filename, not a URL, when filenamemangling"
    This reverts commit 9e65c07737425748e396291d9c20442d11b0641b.
    
    Closes: #1000210
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- lib/Devscripts/Uscan/_xtp.pm
+++ lib/Devscripts/Uscan/_xtp.pm
@@ -11,13 +11,14 @@ our @EXPORT = ('partial_version');
 
 sub _xtp_newfile_base {
     my ($self) = @_;
-    my $newfile_base = basename($self->search_result->{newfile});
-
+    my $newfile_base;
     if (@{ $self->filenamemangle }) {
 
         # HTTP or FTP site (with filenamemangle)
         if ($self->versionless) {
             $newfile_base = $self->upstream_url;
+        } else {
+            $newfile_base = $self->search_result->{newfile};
         }
         my $cmp = $newfile_base;
         uscan_verbose "Matching target for filenamemangle: $newfile_base";
@@ -51,6 +52,7 @@ sub _xtp_newfile_base {
         }
     } else {
         # HTTP or FTP site (without filenamemangle)
+        $newfile_base = basename($self->search_result->{newfile});
         if ($self->mode eq 'http') {
 
             # Remove HTTP header trash
